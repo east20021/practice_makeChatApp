5bbaed879ec962374728a6de5ccff78aa6a9aaa9
commit <>
2017-12-05 20:48:59 +0900

    add LoginView and ChatView

---
+++ practice_makeChatApp/practice_makeChatApp/LogInViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LogInViewController.swift
+//  practice_makeChatApp
+//
+//  Created by lee on 2017. 12. 5..
+//  Copyright © 2017년 smith. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func loginAnonymouslyDidTapped(_ sender: Any) {
+        print("login anonymously did tapped")
+    }
+    
+    @IBAction func googleLoginDidTapped(_ sender: Any) {
+        print("google login did tapped")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
